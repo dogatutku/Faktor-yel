2d0ac11e3048befef894f1442287a30ea98a1568
M. Utku Sarıoğlu <mustafautkusarioglu@gmail.com>
2020-11-04 16:58:37 +0300

    Add project files.

---
+++ FaktoriyelÖdev/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FaktoriyelÖdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayı , i;
+            Console.WriteLine("Lütfen Faktoriyeli Hesaplanacak Sayıyı Giriniz:");
+            sayı = Convert.ToInt32(Console.ReadLine());
+            for ( i = sayı-1; i > 1; i--)
+            {
+                sayı = sayı * i;
+            }
+            Console.WriteLine("İşlem Sonucu :{0}",sayı);
+            Console.ReadLine();
+        }
+    }
+}
